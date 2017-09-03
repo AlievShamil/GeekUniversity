f72d1227e792634161644686f63e13aad409c648
Shamil Aliev <inhuh005@gmail.com>
2017-09-03 17:50:12 +0300

    Сделал GUI на fx, топорно конечно, но для первого раза не судите строго ))

---
+++ src/JavaCore/lesson_4/MainWindow.java
@@ -0,0 +1,27 @@
+package JavaCore.lesson_4;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class MainWindow extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        Scene scene = new Scene(root,400,400);
+        scene.getStylesheets().add(0, "JavaCore/lesson_4/styles/my.css");
+
+        primaryStage.setTitle("FXML Test");
+        primaryStage.getIcons().add(new Image("JavaCore/lesson_4/res/geek.png"));
+        primaryStage.setWidth(400);
+        primaryStage.setHeight(600);
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
